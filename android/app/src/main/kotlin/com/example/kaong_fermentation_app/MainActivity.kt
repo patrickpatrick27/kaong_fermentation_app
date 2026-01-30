8c84dbe9763e2b071f2c3b67a1ed8567ebe58aeb
Dave Patrick I. Bulaso <158995199+patrickpatrick27@users.noreply.github.com>
2026-01-30 16:08:26 +0800

    Version 1.0.7

--- android/app/src/main/kotlin/com/example/kaong_fermentation_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kaong_fermentation_app/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.kaong_fermentation_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
